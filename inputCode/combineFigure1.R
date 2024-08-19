b7e26915c35762ea2c0138ffc1702d9bd79526c8
Jenna Hamlin <jennahamlin@gmail.com>
2024-08-19 11:05:02 -0400

    Delete inputCode/combineFigure1.R

--- inputCode/combineFigure1.R
+++
@@ -1,8 +0,0 @@
-library(cowplot)
-
-pdf(here::here('02_Manuscript/outputFiles', "figure1A-C_RatesandCases.pdf"),
-     width = 12, height = 10)
-
-cowplot::plot_grid(pBig222, pBig1, countMap, nrow = 3, 
-                   labels = c("A", "B", "C"), label_size = 20)
-dev.off()
